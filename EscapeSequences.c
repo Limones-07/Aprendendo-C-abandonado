a3374cdb6c83b70237af6ad822d1aaac2301604e
Limones-07 <07limones@gmail.com>
2022-08-06 17:22:06 -0300

    Escape Sequences (com arquivo compilado p/ Linux)

---
+++ EscapeSequences.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+  /* 
+    Escape sequences:
+      \n = New line
+      \t = Horizontal tab
+
+  */
+
+  printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+
+  printf("\"I like pizza.\" - Abraham Lincoln probably \n");
+
+  /*
+    To print \, " or ':
+      \\ = \
+      \" = "
+      \' = '  
+  */
+
+  /*
+    Other escape sequences:
+      \a = Alert 
+      \b = Backspace
+      \e = Escape character 
+      \f = Formfeed Page Break
+      \r = Carriage return
+      \v = Vertical tab
+      
+  */
+  return 0;
+}
