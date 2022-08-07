d443ecc78a30d9284f03411486c0c9bbe2d818d4
Limones-07 <07limones@gmail.com>
2022-08-06 22:13:37 -0300

    Arithmetics Operators

---
+++ ArithmeticsOperators.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+  // arithmetics operators
+
+  // + (adition)
+  // - (subtraction)
+  // * (multiplication)
+  // / (division)
+  // % (modulus)
+  // ++ (increment)
+  // -- (decrement)
+
+  int x = 5;
+  int y = 2;
+
+  // int z = x + y;
+  // int z = x - y;
+  // int z = x * y;
+  // float z = x / (float) y;
+  // int z = x % y;
+
+  x++;
+  y--;
+
+  printf("%d\n", y);
+
+  return 0;
+}
