f4ddae2598df403c1c9e0b2d446d05e16772fbfb
Limones-07 <07limones@gmail.com>
2022-08-09 19:57:03 -0300

    Arrays

--- Arrays.c
+++ Arrays.c
@@ -4,11 +4,21 @@ int main()
 {
     // array = a data structure that can store many values of some data type.
 
-    double prices[10] = {5.0, 10.0, 15.0, 25.0, 20.0};
+    double prices[] = {5.0, 10.0, 15.0, 25.0, 20.0, 30.0};
 
-    prices[5] = 35.0;
+    printf("%d bytes.\n", sizeof(prices));
+    // number of elements = sizeof(array) / size in bytes of the data type 
+    // ex. double = 8 bytes -> number of elements = sizeof(array of doubles) / 8
+    // to get the size in bytes of the data type use sizeof(array[0])
+    // it will get the size of the element in bytes
+    // so a universal formula for the amount of elements in a array would be
+    // number of elements = sizeof(array)/sizeof(array[0])
 
-    printf("$%.2lf\n", prices[5]);
+    for (int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++)
+    {
+        printf("$%.2lf\n", prices[i]);
+    }
+    
 
     return 0;
 }
