3091eb037db93bc39320b68cec7f926e892a509e
Limones-07 <07limones@gmail.com>
2022-08-08 21:13:31 -0300

    For Loops

---
+++ ForLoops.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    // for loop = repeats a section of code for a limited amount of times
+
+    for (int i = 10; i >= 1; i-=1){
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
